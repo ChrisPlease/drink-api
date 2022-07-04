dfba7d0942717e56c25801d8e110a4531dcdca3b
ChrisPlease <chris@chrisplease.me>
2022-07-04 07:43:15 -0600

    update config

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,9 @@
-export const PORT = process.env.PORT || 4040;
+export const PORT = process.env.PORT || 4040
+
+export const config = {
+  user: process.env.PGUSER as string,
+  host: process.env.PGHOST as string,
+  database: process.env.PGDATABASE as string,
+  password: process.env.PGPASSWORD as string,
+  port: +(process.env.PGPORT as string) || 5432,
+}
