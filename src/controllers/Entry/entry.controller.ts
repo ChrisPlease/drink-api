b3a6448b64e02948b39b22eddf34e8c3a3300e82
ChrisPlease <chris@chrisplease.me>
2022-08-08 19:47:30 -0600

    add find all

--- src/controllers/Entry/entry.controller.ts
+++ src/controllers/Entry/entry.controller.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { ParsedQs } from 'qs'
-import { DateLog, Entry } from '../../models'
-import { CrudController } from '../controller'
+import { DateLog, Drink, Entry, sequelize } from '../../models'
+import { CrudController, ICrudController } from '../controller'
 
 export class EntryController extends CrudController {
   public async create(
@@ -23,19 +23,47 @@ export class EntryController extends CrudController {
       res.status(400).json(err)
     }
   }
-  public read(
+
+  public async read(
+    req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+    res: Response<any, Record<string, any>>,
+  ): Promise<void> {
+    try {
+      const entries = await Entry.findAll({
+        ...(req.query.drinkId ? { where: { drinkId: +req.query.drinkId } } : {}),
+        group: 'drink.name',
+        attributes: [
+          'drink.name',
+          [sequelize.fn('SUM', 'entry.volume'), 'total_volume'],
+          [sequelize.fn('COUNT', sequelize.col('log.entry_timestamp')), 'foo'],
+        ],
+        include: [
+          { model: Drink },
+          { model: DateLog },
+        ],
+      })
+
+      res.json(entries)
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  }
+
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  public readById(
     req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
     res: Response<any, Record<string, any>>,
   ): Promise<void> {
     throw new Error('Method not implemented.')
   }
-  public readById(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>): Promise<void> {
-    throw new Error('Method not implemented.')
-  }
-  public update(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>): Promise<void> {
+  public update(
+    req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+    res: Response<any, Record<string, any>>,
+  ): Promise<void> {
     throw new Error('Method not implemented.')
   }
   public delete(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>>): Promise<void> {
     throw new Error('Method not implemented.')
   }
 }
+/* eslint-enable @typescript-eslint/no-unused-vars */
