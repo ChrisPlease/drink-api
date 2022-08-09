6697984fdbcfc201b8f545f3b70fd39205f619de
ChrisPlease <chris@chrisplease.me>
2022-08-09 16:43:52 -0600

    init session and connect user

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
 import 'dotenv/config'
 import express from 'express'
+import session, { Store } from 'express-session'
 import bodyParser from 'body-parser'
-import { drinkRouter, entryRouter } from './routes'
+import { drinkRouter, entryRouter, userRouter } from './routes'
 import { PORT } from './config/constants'
 import { sequelize } from './models'
+import SequelizeSessionInit from 'connect-session-sequelize'
+
+const SequelizeStore = SequelizeSessionInit(Store)
 
 const app: express.Application = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    store: new SequelizeStore({
+      db: sequelize,
+      tableName: 'sessions',
+    }),
+    resave: false,
+    proxy: true,
+  }),
+)
+
 app.use('/drinks', drinkRouter)
 app.use('/entries', entryRouter)
+app.use('/users', userRouter)
 
 app.get('/', (req, res) => {
   res.json({ info: 'Typescript With Express' })
