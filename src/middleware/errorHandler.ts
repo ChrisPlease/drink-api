119b85ee9811405ff51eb35af921a45d3f477c83
ChrisPlease <chris@chrisplease.me>
2022-08-12 16:16:06 -0600

    fix to errorHandler to trigger json res

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from 'express'
 
-export const errorHandler: ErrorRequestHandler = (err, req, res) => {
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   res.status(err.status).json({ title: err.title, message: err.message })
+  next()
 }
