9606366d29dd672108a64c2154383d21e0be83e3
ChrisPlease <chris@chrisplease.me>
2022-08-11 19:42:01 -0600

    refactor passport

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
+import { AuthError } from '../middleware/authHandler'
 import { User } from '../models/index'
 import { UserModel } from '../models/User.model'
 
@@ -8,27 +9,37 @@ export const router = Router({
   strict: true,
 })
 
-passport.use(new LocalStrategy(async (username, password, cb) => {
+passport.use(new LocalStrategy(async (username, password, done) => {
   try {
-    console.log(username)
     const user = await User.findOne({ where: { username } })
-
-    if (!user || password !== user.password) {
-      return cb(null, false, { message: 'Something went wrong' })
+    if (!user) {
+      return done(new AuthError(400, 'Username does not exist'))
     }
 
-    return cb(null, user)
+    const isAuth = await user?.authenticate(password)
+
+    if (!isAuth) {
+      return done(new AuthError(401, 'Incorrect credentials'))
+    }
+
+    console.log('have the user')
+    return done(null, user)
 
   } catch (err) {
-    return cb({ message: err })
+    return done({ message: `error: ${err}` })
   }
 }))
 
+interface PassportUser {
+  id: number;
+  username: string;
+}
 
 passport.serializeUser((user, done) => {
-  done(null, (user as UserModel).id)
+  done(null, { id: (user as UserModel).id, name: (user as UserModel).username })
 })
 
-passport.deserializeUser(async (id: number, done) => {
-  await User.findByPk(id).then(function(user) { done(null, user) })
+passport.deserializeUser(async ({ id }: PassportUser, done) => {
+  const user = await User.findByPk(id)
+  done(null, user)
 })
