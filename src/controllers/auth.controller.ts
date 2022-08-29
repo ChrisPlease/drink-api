38d286e0db29ff730f1f1c3938daa851989445f7
Chris <chris@chrisplease.me>
2022-08-28 18:07:56 -0600

    refactor: Include User validations, entries on user call (#8)
    * refactor: Include User validations, entries on user call
    
    * move salt rounds to constants
    
    * controller cleanup, log the user in
    
    * cleanup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from 'express'
+import { UniqueConstraintError, ValidationError } from 'sequelize'
 import { User } from '../models'
 
 export class AuthController {
   async signup(req: Request, res: Response): Promise<void> {
     try {
-      await User.create(req.body)
-      res.status(201).json({})
-    } catch (err) {
-      res.status(400).json(err)
+      const user = User.build(req.body)
+
+      await user.save()
+
+      req.login(user, err => {
+        if (err) {
+          res.status(400).json(err)
+        }
+
+        res.status(201).json(user)
+      })
+    } catch (err: any) {
+      if (err instanceof UniqueConstraintError) {
+        const fieldName = Object.keys(err.fields)[0]
+        res.status(409).json({ message: `${fieldName} already taken. Please use a different ${fieldName}.` })
+      } else if (err instanceof ValidationError) {
+        res.status(400).json({ message: err.message })
+      }
     }
   }
 
