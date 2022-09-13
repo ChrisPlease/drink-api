edc96c2828f4ae8f974e450f723a64df5e82112c
ChrisPlease <chris@chrisplease.me>
2022-09-13 08:18:26 -0600

    json api handler middleware

---
+++ src/middleware/jsonApiHandler.ts
@@ -0,0 +1,14 @@
+import type { RequestHandler } from 'express'
+import { dataFormatter } from '../utils/serializer'
+
+export const jsonApiHandler: RequestHandler = async (req, res, next) => {
+  const { headers } = req
+
+  console.log(req.body)
+
+  if (headers['content-type'] === 'application/vnd.api+json') {
+    req.body = dataFormatter.deserialize(req.body)
+  }
+
+  next()
+}
