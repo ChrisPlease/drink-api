cc74d22c25fbfdb5a4a1be5f4dc2839c8a4bd630
ChrisPlease <chris@chrisplease.me>
2022-09-15 07:25:57 -0600

    drop console from middleware

--- src/middleware/jsonApiHandler.ts
+++ src/middleware/jsonApiHandler.ts
@@ -4,8 +4,6 @@ import { dataFormatter } from '../utils/serializer'
 export const jsonApiHandler: RequestHandler = async (req, res, next) => {
   const { headers } = req
 
-  console.log(req.body)
-
   if (headers['content-type'] === 'application/vnd.api+json') {
     req.body = dataFormatter.deserialize(req.body)
   }
