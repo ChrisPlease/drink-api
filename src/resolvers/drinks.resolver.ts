eabb6421f9f6e2b5213993d47dd1626a3e3a6a66
ChrisPlease <chris@chrisplease.me>
2022-09-22 07:39:22 -0600

    update drinks resolver, no asking for ingredients here

--- src/resolvers/drinks.resolver.ts
+++ src/resolvers/drinks.resolver.ts
@@ -6,7 +6,6 @@ export const drinkResolver: GraphQLFieldResolver<any, any, { id: number }> = asy
   prev,
   { id },
 ) => {
-  console.log('resolving drink')
   return await Drink.findByPk(prev.drinkId || id, {
     include: [{
       model: Ingredient,
@@ -24,35 +23,29 @@ export const drinkResolver: GraphQLFieldResolver<any, any, { id: number }> = asy
 export const drinksResolver: GraphQLFieldResolver<any, any, any, any> = async (
   _,
   {
-    first,
-    after,
     search,
+    ...pagination
   },
   ctx,
 ) => {
-  console.log('resolving DRINKS')
   const { rows: drinks, count } = await Drink.findAndCountAll({
     where: {
       ...(search ? { name: { [Op.iLike]: `%${search}%` as string }} : {}),
-      ...(after ? { id: { [Op.gt]: +after } } : {}),
       [Op.or]: [
         { userId: { [Op.is]: null } },
         { userId: ctx.user.id },
       ],
     },
     distinct: true,
-    limit: first,
     order: [['id', 'asc']],
     include: [{ model: Ingredient, as: 'ingredients', through: { attributes: [] } }],
-  }).then(({ rows, count }) => ({ rows: rows.map(d => ({ node: d.toJSON(), cursor: d.id })), count }))
-
-  const lastCursor = drinks[drinks.length - 1]?.cursor
+  })
 
   return {
-    edges: drinks,
+    nodes: drinks,
     pageInfo: {
       records: count,
-      lastCursor,
+      ...pagination,
     },
   }
 }
