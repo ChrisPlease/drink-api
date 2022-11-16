029aad90b915eead03fda8e71f01b89d1d05ce7b
Chris <chris@chrisplease.me>
2022-11-15 20:39:38 -0700

    Containerize api (#13)
    * feat: Add Dockerfile, docker compose and readme
    
    * ignore certs and db
    
    * set up the nginx proxy
    
    * reorg volume
    
    * switch back to volume
    
    * db backup and restore scripts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,9 @@
+export const HOST = process.env.HOST || 'localhost'
 export const PORT = process.env.PORT || 4040
 
 export const dbConfig = {
   user: process.env.PGUSER as string,
-  host: process.env.PGHOST as string,
+  host: process.env.PGHOST || 'http://localhost',
   database: process.env.PGDATABASE as string,
   password: process.env.PGPASSWORD as string,
   port: +(process.env.PGPORT as string) || 5432,
