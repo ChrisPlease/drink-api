840cfcbb2e9966e6a24097ae6a470f3e90650919
ChrisPlease <chris@chrisplease.me>
2023-03-31 20:23:06 -0600

    refactor schema and graphql to include pagination

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 
 async function main() {
   const userId = 'auth0|633cb40c15422d538368f4c6'
   const userId2 = 'auth0|6341da849ae95d74a374a5e1'
 
-  const { id: waterId } = await prisma.drink.create({
+  const { id: waterId } = await delay(5000).then(async () => await prisma.drink.create({
     data: {
       name: 'Water',
       icon: 'glass-water',
@@ -15,7 +16,9 @@ async function main() {
       coefficient: 1,
       sugar: 0,
     },
-  })
+  }))
+
+  await delay(5000)
 
   const { id: coffeeId } = await prisma.drink.create({
     data: {
@@ -27,115 +30,117 @@ async function main() {
     },
   })
 
-  await prisma.drink.createMany({
-    data: [
-      {
-        name: 'Coffee',
-        icon: 'mug-saucer',
-        caffeine: 12,
-        coefficient: 0.7,
-        sugar: 0,
-      },
-      {
-        name: 'Tea',
-        icon: 'mug-tea-saucer',
-        caffeine: 5.875,
-        coefficient: 0.8,
-        sugar: 0,
-      },
-      {
-        name: 'Orange Juice',
-        icon: 'glass-citrus',
-        coefficient: 0.55,
-        sugar: 0.5,
-        caffeine: 0,
-      },
-      {
-        name: 'Milk',
-        icon: 'jug',
-        coefficient: 0.78,
-        caffeine: 0,
-        sugar: 1.5,
-      },
-      {
-        name: 'Wine',
-        icon: 'wine-glass',
-        coefficient: -1.6,
-        caffeine: 0,
-        sugar: 0,
-      },
-      {
-        name: 'Beer',
-        icon: 'beer-mug',
-        coefficient: -0.6,
-        caffeine: 0,
-        sugar: 0.3,
-      },
-      {
-        name: 'Non Alcoholic Beer',
-        icon: 'beer-mug',
-        coefficient: 0.6,
-        caffeine: 0,
-        sugar: 2.375,
-      },
-      {
-        name: 'Vodka',
-        icon: 'martini-glass',
-        coefficient: -3.5,
-        caffeine: 0,
-        sugar: 0,
-      },
-      {
-        name: 'Mineral Water',
-        icon: 'glass-water',
-        coefficient: 0.93,
-        caffeine: 0,
-        sugar: 0,
-      },
-      {
-        name: 'Milkshake',
-        icon: 'blender',
-        sugar: 4.9,
-        coefficient: 0.5,
-        caffeine: 0,
-      },
-      {
-        name: 'Herbal Tea',
-        icon: 'mug-tea-saucer',
-        coefficient: 0.95,
-        caffeine: 3,
-        sugar: 0,
-      },
-      {
-        name: 'Energy Drink',
-        icon: 'can-food',
-        sugar: 2.8,
-        coefficient: 0.4,
-        caffeine: 9,
-      },
-      {
-        name: 'Hot Chocolate',
-        icon: 'mug-marshmallows',
-        sugar: 3,
-        coefficient: 0.4,
-        caffeine: 0.83,
-      },
-      {
-        name: 'Coconut Water',
-        icon: 'glass',
-        sugar: 1,
-        coefficient: 0.85,
-        caffeine: 0,
-      },
-      {
-        name: 'Lemonade',
-        icon: 'glass',
-        sugar: 3,
-        coefficient: 0.8,
-        caffeine: 0,
-      },
-    ],
-  })
+  await delay(5000)
+
+  const drinks = [
+    {
+      name: 'Tea',
+      icon: 'mug-tea-saucer',
+      caffeine: 5.875,
+      coefficient: 0.8,
+      sugar: 0,
+    },
+    {
+      name: 'Orange Juice',
+      icon: 'glass-citrus',
+      coefficient: 0.55,
+      sugar: 0.5,
+      caffeine: 0,
+    },
+    {
+      name: 'Milk',
+      icon: 'jug',
+      coefficient: 0.78,
+      caffeine: 0,
+      sugar: 1.5,
+    },
+    {
+      name: 'Wine',
+      icon: 'wine-glass',
+      coefficient: -1.6,
+      caffeine: 0,
+      sugar: 0,
+    },
+    {
+      name: 'Beer',
+      icon: 'beer-mug',
+      coefficient: -0.6,
+      caffeine: 0,
+      sugar: 0.3,
+    },
+    {
+      name: 'Non Alcoholic Beer',
+      icon: 'beer-mug',
+      coefficient: 0.6,
+      caffeine: 0,
+      sugar: 2.375,
+    },
+    {
+      name: 'Vodka',
+      icon: 'martini-glass',
+      coefficient: -3.5,
+      caffeine: 0,
+      sugar: 0,
+    },
+    {
+      name: 'Mineral Water',
+      icon: 'glass-water',
+      coefficient: 0.93,
+      caffeine: 0,
+      sugar: 0,
+    },
+    {
+      name: 'Milkshake',
+      icon: 'blender',
+      sugar: 4.9,
+      coefficient: 0.5,
+      caffeine: 0,
+    },
+    {
+      name: 'Herbal Tea',
+      icon: 'mug-tea-saucer',
+      coefficient: 0.95,
+      caffeine: 3,
+      sugar: 0,
+    },
+    {
+      name: 'Energy Drink',
+      icon: 'can-food',
+      sugar: 2.8,
+      coefficient: 0.4,
+      caffeine: 9,
+    },
+    {
+      name: 'Hot Chocolate',
+      icon: 'mug-marshmallows',
+      sugar: 3,
+      coefficient: 0.4,
+      caffeine: 0.83,
+    },
+    {
+      name: 'Coconut Water',
+      icon: 'glass',
+      sugar: 1,
+      coefficient: 0.85,
+      caffeine: 0,
+    },
+    {
+      name: 'Lemonade',
+      icon: 'glass',
+      sugar: 3,
+      coefficient: 0.8,
+      caffeine: 0,
+    },
+  ]
+
+  for (let i = 0; i < drinks.length; i++) {
+    await delay(5000).then(async () => {
+      console.log('foooo', i, new Date().toISOString())
+      await prisma.drink.create({ data: drinks[i] })
+    })
+  }
+
+  await delay(5000)
 
   const { id: sodaId } = await prisma.drink.create({
     data: {
@@ -146,6 +151,9 @@ async function main() {
       sugar: 3.25,
     },
   })
+
+  await delay(5000)
+
   const { id: whiskeyId } = await prisma.drink.create({
     data:
     {
@@ -157,12 +165,16 @@ async function main() {
     },
   })
 
+  await delay(5000)
+
   await prisma.user.createMany({
     data: [
       { id: userId },
       { id: userId2 },
     ],
   })
+
+  await delay(5000)
 
   const { id } = await prisma.drink.create({
     data: {
@@ -200,18 +212,22 @@ async function main() {
     },
   })
 
-  await prisma.entry.createMany({
-    data: [
-      { drinkId: sodaId, userId, volume: 12 },
-      { drinkId: sodaId, userId, volume: 12 },
-      { drinkId: whiskeyId, userId, volume: 1.5 },
-      { drinkId: whiskeyId, userId, volume: 1.5 },
+  const entryData = [
+    { drinkId: sodaId, userId, volume: 12, timestamp: new Date() },
+    { drinkId: sodaId, userId, volume: 12 },
+    { drinkId: whiskeyId, userId, volume: 1.5 },
+    { drinkId: whiskeyId, userId, volume: 1.5 },
 
-      { drinkId: waterId, userId: userId2, volume: 28 },
-      { drinkId: waterId, userId: userId2, volume: 32 },
-      { drinkId: coffeeId, userId: userId2, volume: 12 },
-    ],
-  })
+    { drinkId: waterId, userId: userId2, volume: 28 },
+    { drinkId: waterId, userId: userId2, volume: 32 },
+    { drinkId: coffeeId, userId: userId2, volume: 12 },
+  ]
+
+  for (let i = 0; i < entryData.length; i++) {
+
+    await delay(5000)
+    await prisma.entry.create({ data: {...entryData[i], timestamp: new Date() } })
+  }
 }
 
 
