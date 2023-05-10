48c7491b2d5a551a6dfd1005566a8708574e7350
ChrisPlease <chris@chrisplease.me>
2023-05-10 07:19:34 -0600

    general cleanup, remove unused vars, delete unused types file and constants file

--- src/constants/db.ts
+++
@@ -1,8 +0,0 @@
-
-export const dbConfig = {
-  user: process.env.PGUSER as string,
-  host: process.env.PGHOST as string,
-  database: process.env.PGDATABASE as string,
-  password: process.env.PGPASSWORD as string,
-  port: +(process.env.PGPORT as string),
-}
