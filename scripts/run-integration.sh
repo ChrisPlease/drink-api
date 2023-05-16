05cbcf5aeee46314116b2737c7418abd73a7362a
ChrisPlease <chris@chrisplease.me>
2023-05-15 21:06:09 -0600

    run migration for integration tests

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -7,7 +7,7 @@ docker compose -f docker-compose.test.yml up -d
 echo '🟡 - Waiting for database to be ready...'
 
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
-npx prisma db push
+npx prisma migrate dev --name init
 if [ "$#" -eq  "0" ]
   then
     vitest -c ./vitest.config.integration.ts
