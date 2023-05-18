e4f990bac047332311d0f9c29fdf3b89340a0e44
ChrisPlease <chris@chrisplease.me>
2023-05-18 07:11:27 -0600

    pass in args for integration tests

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -8,9 +8,5 @@ echo '🟡 - Waiting for database to be ready...'
 
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
-if [ "$#" -eq  "0" ]
-  then
-    vitest -c ./vitest.config.integration.ts
-else
-    vitest -c ./vitest.config.integration.ts --ui
-fi
+
+vitest -c ./vitest.config.integration.ts "$@";
