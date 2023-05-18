d785e87bd08bdfe80174eba496836521ff1eed39
ChrisPlease <chris@chrisplease.me>
2023-05-18 07:11:44 -0600

    fix unit config

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: [
+      'src/**/*.test.ts',
+      '!src/tests/**',
+    ],
     coverage: {
       all: true,
       include: [
         'src/**/*.test.ts',
-        '!src/tests',
+        '!src/tests/**',
       ],
       exclude: [
+        'src/tests/**',
         'src/types/*',
         'src/**/*.test.ts',
         'prisma/*',
