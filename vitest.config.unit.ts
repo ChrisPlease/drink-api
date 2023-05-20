12660ea7d4a50bfac305b1720a5341cff2e18dd8
ChrisPlease <chris@chrisplease.me>
2023-05-20 07:14:04 -0600

    fix vitest config

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -4,8 +4,28 @@ export default defineConfig({
   test: {
     include: [
       'src/**/*.test.ts',
-      '!src/tests/**',
     ],
+    exclude: [
+      'src/tests/**/*',
+    ],
+    coverage: {
+      all: true,
+
+      include: [
+        'src/**/*.ts',
+        '!src/**/*.test.ts',
+      ],
+      exclude: [
+        'src/index.ts',
+        'src/client.ts',
+        'src/tests',
+        'src/__mocks__',
+        'src/__generated__',
+        'src/middleware',
+      ],
+      reporter: ['html', 'json', 'lcov'],
+      provider: 'istanbul',
+    },
     alias: {
       '@': 'src',
     },
