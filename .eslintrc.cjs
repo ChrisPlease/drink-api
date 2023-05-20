0627039063aba8902e02f4708bf3b37f1be3595d
ChrisPlease <chris@chrisplease.me>
2023-05-20 08:18:29 -0600

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,82 +1,88 @@
-/** @type {import('eslint').Linter.Config} */
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  'ignorePatterns': ['src/__generated__/**/*'],
-  'overrides': [
+  "ignorePatterns": ["src/__generated__/**/*"],
+  "rules": {
+
+  },
+  "overrides": [
     {
-      'files': ['*.gql'],
-      'extends': 'plugin:@graphql-eslint/schema-recommended',
-      'parserOptions': {
-        'schema': './schema.gql',
+      "files": ["*.gql"],
+      "extends": "plugin:@graphql-eslint/schema-recommended",
+      "parserOptions": {
+        "schema": "./schema.gql",
       },
-      'rules': {
-        '@graphql-eslint/strict-id-in-types': ['error', {
-          'exceptions': {
-            'types': ['PageInfo'],
-            'suffixes': ['Paginated', 'Edge'],
+      "rules": {
+        "@graphql-eslint/strict-id-in-types": ["error", {
+          "exceptions": {
+            "types": ["PageInfo"],
+            "suffixes": ["Paginated", "Edge"],
           },
         }],
       },
     },
     {
-      'files': ['*.ts'],
-      'env': {
-        'es2021': true,
-        'node': true,
+      "files": ["**/*.ts"],
+      "env": {
+        "es2021": true,
+        "node": true,
       },
-      'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
+      "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
       ],
-      'parser': '@typescript-eslint/parser',
-      'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module',
-        'tsconfigRootDir': './'
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "tsconfigRootDir": "./"
       },
-      'plugins': [
-        '@typescript-eslint',
-        'import',
+      "plugins": [
+        "@typescript-eslint",
+        "import",
       ],
-      'rules': {
-        // 'import/no-unresolved': 'error',
-        'semi': 'off',
-        'comma-dangle': 'off',
-        'import/order': ['error'],
+      "rules": {
+        "semi": "off",
+        "comma-dangle": "off",
+        "import/order": ["error"],
 
-        'quotes': ['error', 'single', { 'avoidEscape': true }],
+        "quotes": ["error", "single", { "avoidEscape": true }],
 
         // Typescript rules
-        '@typescript-eslint/semi': ['error', 'never'],
-        '@typescript-eslint/member-delimiter-style': ['error', {
-          'multiline': {
-            'delimiter': 'comma',
-            'requireLast': true,
+        "@typescript-eslint/semi": ["error", "never"],
+        "@typescript-eslint/member-delimiter-style": ["error", {
+          "multiline": {
+            "delimiter": "comma",
+            "requireLast": true,
           },
-          'overrides': {
-            'interface': {
-              'multiline': {
-                'delimiter': 'semi',
-                'requireLast': true,
+          "overrides": {
+            "interface": {
+              "multiline": {
+                "delimiter": "semi",
+                "requireLast": true,
               },
             },
           },
 
         }],
-        '@typescript-eslint/no-explicit-any': ['off'],
-        '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+        "@typescript-eslint/no-unused-vars": ["warn", {
+          "destructuredArrayIgnorePattern": "^_",
+          "ignoreRestSiblings": true,
+        }],
+        "@typescript-eslint/no-explicit-any": ["off"],
+        "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
       },
-      'settings': {
-        'import/resolver': {
-          'typescript': {
-            'alwaysTryTypes': true,
-            'project': '<root>/tsconfig.json'
+      "settings": {
+        "import/resolver": {
+          "typescript": {
+            "alwaysTryTypes": true,
+            "project": "<root>/tsconfig.json"
           },
         },
-        'import/parsers': {
-          '@typescript-eslint/parser': ['.ts', '.tsx'],
+        "import/parsers": {
+          "@typescript-eslint/parser": [".ts", ".tsx"],
         },
       },
     },
