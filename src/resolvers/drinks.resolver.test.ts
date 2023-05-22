96e89fc246ed6bd7c202eab9be0a935e50a393c6
ChrisPlease <chris@chrisplease.me>
2023-05-22 12:44:04 -0600

    refactor drink resolvers

--- src/resolvers/drinks.resolver.test.ts
+++ src/resolvers/drinks.resolver.test.ts
@@ -12,11 +12,22 @@ import { Drink } from '@prisma/client'
 import prisma from '../__mocks__/prisma'
 import { toCursorHash } from '../utils/cursorHash'
 import { AppContext } from '../types/context'
+import { Drinks } from '../models/Drink.model'
 import {
   drinkResolvers,
+  mixedDrinkResolvers,
 } from './drinks.resolver'
 
-describe('drinkResolvers', () => {
+vi.mock('../models/Drink.model', () => ({
+  Drinks: vi.fn().mockReturnValue({
+    ...prisma.drink,
+    findDrinkEntries: vi.fn().mockReturnValue([{ id: 'mock-entry' }]),
+    findDrinkUser: vi.fn().mockReturnValue({ id: 'user-123' }),
+    findDrinkIngredients: vi.fn().mockReturnValue([{ id: '123' }]),
+  }),
+}))
+
+describe('drinks.resolver', () => {
   const args = {}
   let ctx: AppContext
   const info = {} as GraphQLResolveInfo
@@ -32,27 +43,87 @@ describe('drinkResolvers', () => {
       } as Request,
       res: {} as Response,
     } as AppContext
-    parent = {
-      id: toCursorHash('BaseDrink:123'),
-      name: 'Test Drink',
-      icon: 'test-icon',
-    } as Drink
-
-    prisma.drink.findUnique.mockResolvedValue({
-      ...parent,
-      entries: vi.fn().mockResolvedValue([]),
-    } as Drink & { entries: any })
   })
 
-  describe.skip('entries', () => {
-    test('returns something', async () => {
-      await drinkResolvers.entries?.(
-        parent,
-        args,
-        ctx,
-        info,
-      )
-      expect(true).toBe(true)
+  describe('drinkResolvers', () => {
+    beforeEach(() => {
+      parent = {
+        id: toCursorHash('BaseDrink:123'),
+        name: 'Test Drink',
+        icon: 'test-icon',
+      } as Drink
+    })
+
+    describe('__resolveType', () => {
+      test('returns the proper typename dehashed from the id', async () => {
+        const res = await drinkResolvers.__resolveType(parent)
+        expect(res).toEqual('BaseDrink')
+      })
+    })
+
+    describe('entries', () => {
+      test('makes a call to Drink model to return entries', async () => {
+        expect.assertions(3)
+        const res = await drinkResolvers.entries?.(
+          parent,
+          args,
+          ctx,
+          info,
+        )
+
+        expect(Drinks).toHaveBeenCalledWith(prisma.drink)
+        expect(
+          Drinks(prisma.drink).findDrinkEntries,
+        ).toHaveBeenCalledWith(parent.id, 'mock-user')
+        expect(res).toStrictEqual([{ id: 'mock-entry' }])
+      })
+    })
+
+    describe('user', () => {
+      test('makes a call to Drink model to return the user', async () => {
+        expect.assertions(3)
+        const res = await drinkResolvers.user?.(
+          parent,
+          args,
+          ctx,
+          info,
+        )
+
+        expect(Drinks).toHaveBeenCalledWith(prisma.drink)
+        expect(
+          Drinks(prisma.drink).findDrinkUser,
+        ).toHaveBeenCalledWith(parent.id)
+        expect(res).toStrictEqual({ id: 'user-123' })
+      })
+    })
+  })
+
+  describe('mixedDrinkResolvers', () => {
+    beforeEach(() => {
+      parent = {
+        id: toCursorHash('MixedDrink:123'),
+        name: 'Test Drink',
+        icon: 'test-icon',
+      } as Drink
+    })
+
+    describe('ingredients', () => {
+      test('makes a call to Drink model to return ingredients', async () => {
+        expect.assertions(3)
+        const res = await mixedDrinkResolvers.ingredients?.(
+          parent,
+          args,
+          ctx,
+          info,
+        )
+
+        expect(Drinks).toHaveBeenCalledWith(prisma.drink)
+        expect(
+          Drinks(prisma.drink).findDrinkIngredients,
+        ).toHaveBeenCalledWith(parent.id)
+        expect(res).toStrictEqual([{ id: '123' }])
+      })
     })
   })
 })
+
