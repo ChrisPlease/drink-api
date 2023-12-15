b45bd89a37b9cdf31f20bc32feb70e5de0cbc929
ChrisPlease <chris@chrisplease.me>
2023-12-14 20:27:40 -0700

    fix types

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { PrismaClient, Prisma } from '@prisma/client'
 import { createSoftDeleteExtension } from 'prisma-extension-soft-delete'
 
-const isDev = process.env.NODE_ENV === 'develop'
+const isDev = process.env.NODE_ENV === 'development'
 
 const prisma = new PrismaClient({
   log: <Prisma.LogLevel[]>['info', 'error'].concat(isDev ? [] : []),
