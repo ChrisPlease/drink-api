63c4ab01c2d2d15382138f83ea351653dd4df658
ChrisPlease <chris@chrisplease.me>
2024-01-23 19:25:32 -0700

    monorepo: prep for pnpm

--- vitest.config.integration.ts
+++ vitest.config.integration.ts
@@ -1,33 +1,36 @@
 import path from 'path'
-import { defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
 
-export default defineConfig({
-  test: {
-    include: ['functions/**/tests/**/*.test.ts'],
-    threads: false,
-    setupFiles: ['functions/graphql/tests/helpers/setup.ts'],
-    alias: {
-      '@': path.resolve(__dirname, './functions'),
+export default mergeConfig(
+  {},
+  defineConfig({
+    test: {
+      include: ['functions/**/tests/**/*.test.ts'],
+      threads: false,
+      setupFiles: ['functions/graphql/src/tests/helpers/setup.ts'],
+      alias: {
+        '@': path.resolve(__dirname, './functions'),
+      },
+      coverage: {
+
+        all: true,
+        reportsDirectory: './coverage/integration',
+        include: [
+          'functions/**/*.ts',
+          '!functions/**/*.test.ts',
+
+        ],
+        exclude: [
+          'functions/**/tests/*',
+          'functions/**/*.test.ts',
+          'functions/**/index.ts',
+          'functions/**/client.ts',
+          'functions/**/__mocks__',
+          'functions/**/__generated__',
+        ],
+        reporter: ['html', 'json', 'lcov', 'text', 'text-summary'],
+        provider: 'istanbul',
+      },
     },
-    coverage: {
-
-      all: true,
-      reportsDirectory: './coverage/integration',
-      include: [
-        'functions/**/*.ts',
-        '!functions/**/*.test.ts',
-
-      ],
-      exclude: [
-        'functions/**/tests/*',
-        'functions/**/*.test.ts',
-        'functions/**/index.ts',
-        'functions/**/client.ts',
-        'functions/**/__mocks__',
-        'functions/**/__generated__',
-      ],
-      reporter: ['html', 'json', 'lcov', 'text', 'text-summary'],
-      provider: 'istanbul',
-    },
-  },
-})
+  }),
+)
