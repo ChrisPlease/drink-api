713aa590e846453ea3bb5e83768ca100341209a4
ChrisPlease <chris@chrisplease.me>
2024-04-03 19:39:52 -0600

    eslint uses packages tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,7 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module",
         "tsConfigRootDir": "./",
-        "project": ["./tsconfig.eslint.json", "./functions/*/tsconfig.json"]
+        "project": ["./packages/tsconfig/base.json", "./functions/*/tsconfig.json"]
       },
       "plugins": [
         "@typescript-eslint",
@@ -67,7 +67,7 @@ module.exports = {
         "import/resolver": {
           "typescript": {
             "alwaysTryTypes": true,
-            "project": ["<root>/tsconfig.eslint.json", "./functions/*/tsconfig.json"]
+            "project": ["./packages/tsconfig/base.json", "./functions/*/tsconfig.json"]
           },
         },
         "import/parsers": {
