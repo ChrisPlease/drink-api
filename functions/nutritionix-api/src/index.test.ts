ccfa43d53dcb5f98e9c813c701ee6dca091f9c70
ChrisPlease <chris@chrisplease.me>
2024-04-13 17:11:27 -0600

    fix nutritionix tests

--- functions/nutritionix-api/src/index.test.ts
+++ functions/nutritionix-api/src/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { MockAgent, setGlobalDispatcher } from 'undici'
 import { Context } from 'aws-lambda'
 import { handler } from '.'
@@ -10,15 +10,32 @@ setGlobalDispatcher(mockAgent)
 const mockPool = mockAgent.get(`${process.env.NUTRITIONIX_API}`)
 
 mockPool.intercept({
-  path: '/search/item',
+  path: '/v2/search/item',
+  method: 'GET',
+  query: {
+    upc: '123',
+  },
+  headers: {
+    'x-app-id': process.env.NUTRITIONIX_APP_ID,
+    'x-app-key': process.env.NUTRITIONIX_API_KEY,
+  },
+}).reply(200, {
+  status: { ok: true },
+  message: 'transaction processed',
 })
 
 describe('this', () => {
+  let ctx: Context
+  const cb = () => {}
+
+  beforeEach(() => {
+    ctx = {} as Context
+  })
   test('this', async () => {
     // console.log(process.env.NUTRITIONIX_API)
-    // const foo = await handler({ upc: '123' }, {} as Context, () => {})
+    const foo = await handler({ upc: '123' }, ctx, cb)
 
-    // console.log(foo)
+    console.log(foo)
     expect(true).toBeTruthy()
   })
 })
--- functions/nutritionix-api/src/index.ts
+++ functions/nutritionix-api/src/index.ts
@@ -1,5 +1,5 @@
 import { Handler } from 'aws-lambda'
-import { Headers, fetch } from 'undici'
+import { fetch as undiciFetch, Headers } from 'undici'
 import * as dotEnv from 'dotenv'
 import { ApiError, Logger } from '@waterlog/utils'
 import {
@@ -27,7 +27,7 @@ async function baseFetch(path: string, params: Record<string, string>): Promise<
   headers.set('x-app-id', process.env.NUTRITIONIX_APP_ID)
   headers.set('x-app-key', process.env.NUTRITIONIX_API_KEY)
 
-  const res = await fetch(`${process.env.NUTRITIONIX_API}/v2/${path}?${queryParams.toString()}`, {
+  const res = await undiciFetch(`${process.env.NUTRITIONIX_API}/v2/${path}?${queryParams.toString()}`, {
     headers,
   })
 
@@ -44,6 +44,8 @@ async function fetchItem<T>(params: { upc: string }): Promise<T | undefined> {
   try {
 
     const res = await baseFetch('search/item', params)
+
+    console.log('RES', res)
     const item = res?.foods?.[0]
 
     if (item)
