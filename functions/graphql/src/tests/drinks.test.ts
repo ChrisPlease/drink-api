880f3d07dcbf683306b39f63de03912e00828962
ChrisPlease <chris@chrisplease.me>
2025-01-12 11:28:54 -0700

    drop only modifier

--- functions/graphql/src/tests/drinks.test.ts
+++ functions/graphql/src/tests/drinks.test.ts
@@ -15,7 +15,7 @@ import { DrinkResult, DrinksPaginated, MixedDrink } from '../__generated__/graph
 import prisma from './helpers/prisma'
 import { testServer } from './helpers/server'
 
-describe.only('drinks', () => {
+describe('drinks', () => {
   let contextValue: AppContext
   let QUERY: DocumentNode
   let waterId: string
